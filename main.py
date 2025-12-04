d8ddfe5e47c962a0c2dccd290d36daa8f92bbd19
Ariel <arielhalifa@gmail.com>
2025-12-04 12:12:59 +0200

    finish exam

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 import pandas as pd
+from utils import Functions as f
 
+
+def run_all(df):
+    # level 1
+    df = f.converting_types(df)
+    # level 2
+    df = f.removing_html(df)
+    #level 3
+    df = f.no_coupon(df)
+    # level 4
+    df = f.adding_month_column(df)
+    #level 5
+    df = f.adding_high_value_order_column(df)
+    #level 6
+    df = f.adding_avg_rating(df)
+    #level 7
+    df = f.sorting_columns(df)
+    #level 8
+    df = f.creatin_delivery_status(df)
+
+
+if __name__ == "__main__":
+    df = f.read_json('orders_simple.json')
+    df = run_all(df)
+    #level 9
+    df.to_csv('clean_orders_[209643808].csv', index = False)
+    print('csv file create succefully')
--- utils.py
+++ utils.py
@@ -21,11 +21,13 @@ class Functions:
         df['shipping_days'] = pd.to_numeric(df['shipping_days']) #already int but for practice i convert again beased on the teacher
         df['customer_age'] = pd.to_numeric(df['customer_age']) #already int but for practice i convert again based on the teacher
         df['order_date'] = pd.to_datetime(df['order_date'])
-
+        print(df.dtypes)
 
     @staticmethod
     def removing_html(df):
-        df['items_html'] = df['items_html'].str.replace(r'<[^<>]*>', ' ', regex = True)
+        df['items_html'] = df['items_html'].str.replace('<br>', ' ')
+        df['items_html'] = df['items_html'].str.replace('<b>', '')
+        df['items_html'] = df['items_html'].str.replace('</b>', '')
         return df
     
     @staticmethod
@@ -41,7 +43,9 @@ class Functions:
     
     @staticmethod
     def adding_high_value_order_column(df):
-        df = df.assign(high_value_order = lambda x: True if x['total_amount'] > x['total_amount'].mean() else False)
+        avg_total = df['total_amount'].mean()
+        mask = df['total_amount'] > avg_total
+        df['high_value_order'] = mask
         return df.sort_values('total_amount', ascending = False)
     
 
